d460eb20584d60b18de5096e68abdc73c0426fb2
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-09-02 10:16:38 -0300

    Update Exercicio1.py
    nesse commit dei um espaço

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,6 +1,6 @@
 n1 = float(input("Digite a primeira nota "))
 n2 = float(input("Digite a segunda nota "))
-n2 = float(input("Digite a terceira nota"))
+n3 = float(input("Digite a terceira nota "))
 n4 = float(input("Digite a quarta nota "))
 
 media = (n1 + n2 + n3 + n4) / 4
