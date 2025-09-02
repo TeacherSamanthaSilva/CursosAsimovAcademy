4a76d747478bbf51e572eb0c5edf43d84d7b732f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-09-02 12:30:00 -0300

    Create Exercicio3.py
    nesse commit conclui o primeiro desafio

---
+++ Exercicio3.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite o seu nome "))
+idade = int(input("Digite a sua idade "))
+print(f"Oi {nome}")
+quantidadedeletras = len(nome) #cinta quantas letras tem o nome
+print(f"Meu nome tem {quantidadedeletras} letras")
+idadedaquicincoanos = idade + 5
+print(f"Minha idade daqui cinco anos será: {idadedaquicincoanos}")
